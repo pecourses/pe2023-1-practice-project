2b4b9477925b064046d34a7bd8053459533ba482
Viktoriia <pecourses>
2024-02-19 00:25:17 +0200

    Refactor model associations

--- server/src/models/Rating.js
+++ server/src/models/Rating.js
@@ -1,30 +1,36 @@
-
-
 module.exports = (sequelize, DataTypes) => {
-  const Rating = sequelize.define('Ratings', {
-    offerId: {
-      allowNull: false,
-      primaryKey: true,
-      type: DataTypes.INTEGER,
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-    },
-    mark: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: 0,
-        max: 5,
+  const Rating = sequelize.define(
+    'Ratings',
+    {
+      offerId: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      mark: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+          max: 5,
+        },
       },
     },
-  },
-  {
-    timestamps: false,
-  });
+    {
+      timestamps: false,
+    }
+  );
+
+  Rating.associate = function (models) {
+    Rating.belongsTo(models.Users, { foreignKey: 'userId' });
+    Rating.belongsTo(models.Offers, { foreignKey: 'offerId' });
+  };
 
   return Rating;
 };
