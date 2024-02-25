91c1f513f0504fd0e2ab4193cf19abeb4f108683
Viktoriia <pecourses>
2024-02-25 15:36:27 +0200

    Add transactions migration

---
+++ server/src/migrations/20240225132408-create-Transactions.js
@@ -0,0 +1,47 @@
+'use strict';
+const { Op } = require('sequelize');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      operationType: {
+        type: Sequelize.ENUM('INCOME', 'EXPENCE'),
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      summ: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.addConstraint('Transactions', {
+      fields: ['summ'],
+      type: 'check',
+      where: { summ: { [Op.gt]: 0 } },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Transactions');
+  },
+};
