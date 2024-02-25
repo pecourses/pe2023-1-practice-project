b7f3b275bec97eb532c43481d43499586601c55a
Viktoriia <pecourses>
2024-02-25 18:29:20 +0200

    Transaction Page with header

--- client/src/pages/TransactionsPage/TransactionsPage.jsx
+++ client/src/pages/TransactionsPage/TransactionsPage.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 
 function TransactionsPage () {
-  return <div>TransactionsPage</div>;
+  return (
+    <>
+      <Header />
+      <div>TransactionsPage</div>
+    </>
+  );
 }
 
 export default TransactionsPage;
